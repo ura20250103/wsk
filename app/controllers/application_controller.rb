ba1f113bfeea7a7f75dd0e17b6f1eba7548f3955
ura0328 <ura20250103@gmail.com>
2020-04-19 12:43:44 +0000

    user edit create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :email, :user_name, :password, :password_confirmation ]
+    added_attrs = [ :email, :user_name, :user_kana_name, :password, :password_confirmation ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs

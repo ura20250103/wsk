b0749e7bf237736cbca534093e4397d9af1e28cc
ura0328 <ura20250103@gmail.com>
2020-04-19 09:56:44 +0000

    user show adjust

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+  
   end
 
   def edit
   end
   
     def update
-     
+       user = User.find(hogehoge_id)
+       if user.update(user_params)
+          redicrect_to hogehoge_path(hogehoge_id), notice: 'success!'
+       else
+         render :new
+      end
    end
   
    
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_18_084332) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_name"
+    t.string "user_name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

966472a1eb0f7dabd61039438c181e737d8d55eb
ura0328 <ura20250103@gmail.com>
2020-04-20 04:33:29 +0000

    catrgory create

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,26 @@
+class CategoriesController < ApplicationController
+  def index
+    @category = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+      if @category.save
+        @category = Category.all
+        redirect_to categories_path
+      else
+        redirect_to new_category_path
+      end
+    end
+
+    private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
+
+
